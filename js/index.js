63469044eb67861dd572d5375ce6740f5a941476
OlhaNazarova <108295915+OlhaNazarova@users.noreply.github.com>
2024-03-07 17:32:28 +0200

    lesson js test

---
+++ js/index.js
@@ -0,0 +1,11 @@
+const person = {
+    name: 'Bob',
+    age: 25,
+    location = {
+        city: 'Poltava',
+        street: 'Orlyk',
+        home: 10,
+    }
+}
+console.log(person);
+console.log(14);
